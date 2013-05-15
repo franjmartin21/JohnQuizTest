64bf7b0768fb177107d162fbd1de8c56c59b4a26
Fran <franjmartin21@gmail.com>
2013-05-15 21:03:03 +0200

    #7 Aniadido test para el generador de numeros aleatorios

--- src/com/johnandroid/quiz/services/JohnQuizServiceImplTest.java
+++ src/com/johnandroid/quiz/services/JohnQuizServiceImplTest.java
@@ -2,9 +2,10 @@ package com.johnandroid.quiz.services;
 
 import java.util.List;
 
-import com.johnandroid.quiz.domain.Pregunta;
-
 import android.test.AndroidTestCase;
+import android.util.Log;
+
+import com.johnandroid.quiz.domain.Pregunta;
 
 public class JohnQuizServiceImplTest extends AndroidTestCase{
 	
@@ -15,9 +16,18 @@ public class JohnQuizServiceImplTest extends AndroidTestCase{
 		johnQuizServiceImpl = new JohnQuizServiceImpl(getContext());
 	}
 	
+
 	public void testGetPreguntas(){
 		List <Pregunta>preguntas = johnQuizServiceImpl.getPreguntas(2);
 		assertEquals(2, preguntas.size());
 		assertEquals(3, preguntas.get(0).getRespuestas().size());
 	}
+	/* Comentado ya que he hecho el metodo privado
+	public void testRandomGenerator(){
+		for(int i = 0; i < 100; i++){
+			int numgen = johnQuizServiceImpl.getRandomId(10);
+			assertTrue(numgen >= 0);
+			assertTrue(numgen < 10);
+		}
+	}*/
 }
